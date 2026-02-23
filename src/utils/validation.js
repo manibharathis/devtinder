ed6e1e6bfba5537a128e8a7528cdbc3df921100b
manibharathis <remasekar.96@gmail.com>
2026-02-23 12:32:14 +0530

    bug fix

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,7 +2,7 @@ const validator = require("validator")
 const validateSignUpData = (body)=>{
 const {firstName,lastName,email,password} = body
   if(!firstName || !lastName){
-    throw new EncodedAudioChunkrror("name is not valid!")
+    throw new Error("name is not valid!")
   }
   else if(!validator.isEmail(email)){
     throw new Error("email is not valid")
